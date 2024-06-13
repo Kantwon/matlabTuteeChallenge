3d01c0b732294759249cf6a88db1d5d14add96f5
Kantwon <39605001+Kantwon@users.noreply.github.com>
2024-06-13 17:56:28 +0000

    fixing logging

--- Chatbot.py
+++ Chatbot.py
@@ -11,9 +11,27 @@ from st_files_connection import FilesConnection
 from google.cloud import storage
 import os
 import io
+from google.oauth2 import service_account
+
+gcp_credentials = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"],
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
+}
+
+credentials = service_account.Credentials.from_service_account_info(gcp_credentials)
+client_bucket = storage.Client(credentials=credentials, project=gcp_credentials["project_id"])
+
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ".streamlit/credential.json"
-client_bucket = storage.Client()
+#client_bucket = storage.Client()
 bucket_name = "matlabtuteesummer2024_bucket"
 file_name = "matlabTuteeSummer2024_Transcripts.csv"
 
